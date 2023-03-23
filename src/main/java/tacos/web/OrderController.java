45e730c6c9a4d2842dcbc69403fcfe1ce2b20337
JOBFER1 <jobfer@outlook.es>
2023-03-23 16:50:36 +0100

    Hacer que aparezcan ya rellenos los datos de facturación del pedido.
    Los coge de los datos del usuario logueado.

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -2,9 +2,9 @@ package tacos.web;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -29,8 +29,24 @@ public class OrderController {
 	}
 	
 	@GetMapping("/current")
-	public String orderForm(Model model) {
+	public String orderForm(@AuthenticationPrincipal User user, @ModelAttribute TacoOrder tacoOrder) {	
 		
+	    if (tacoOrder.getDeliveryName() == null) {
+	    	tacoOrder.setDeliveryName(user.getFullname());
+	      }
+	      if (tacoOrder.getDeliveryStreet() == null) {
+	    	  tacoOrder.setDeliveryStreet(user.getStreet());
+	      }
+	      if (tacoOrder.getDeliveryCity() == null) {
+	    	  tacoOrder.setDeliveryCity(user.getCity());
+	      }
+	      if (tacoOrder.getDeliveryState() == null) {
+	    	  tacoOrder.setDeliveryState(user.getState());
+	      }
+	      if (tacoOrder.getDeliveryZip() == null) {
+	    	  tacoOrder.setDeliveryZip(user.getZip());
+	      }
+	      
 		return "orderForm";
 	}
 	
